f03218a703bb1c5a8e17f81a7526a4dd6f5ca32f
flaviocapacciocnh <flavio.capaccio@external.cnh.com>
2013-01-31 18:00:16 +0100

    Todo: rewrite setMax function and function to increase and decrease value on click on left and right arrow buttons.

--- SeekBarTest/gen/com/flaviocapaccio/seekbartest/R.java
+++ SeekBarTest/gen/com/flaviocapaccio/seekbartest/R.java
@@ -26,10 +26,14 @@ public final class R {
     public static final class id {
         public static final int leftArrowButton=0x7f080000;
         public static final int maxProgressValueView=0x7f080005;
-        public static final int menu_settings=0x7f080006;
+        public static final int maxValueInput=0x7f080008;
+        public static final int menu_settings=0x7f08000a;
         public static final int minProgressValueView=0x7f080004;
+        public static final int minValueInput=0x7f080006;
         public static final int rightArrowButton=0x7f080003;
         public static final int seekBar=0x7f080001;
+        public static final int setMaxButton=0x7f080009;
+        public static final int setMinButton=0x7f080007;
         public static final int tvProgress=0x7f080002;
     }
     public static final class layout {
@@ -43,6 +47,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int invalid_input_value=0x7f050003;
         public static final int menu_settings=0x7f050002;
     }
     public static final class style {
--- SeekBarTest/src/com/flaviocapaccio/seekbartest/MainActivity.java
+++ SeekBarTest/src/com/flaviocapaccio/seekbartest/MainActivity.java
@@ -2,27 +2,43 @@ package com.flaviocapaccio.seekbartest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	TextView progressTv = null;
-	SeekBar seekBar = null;
+	TextView progressTv;
+	TextView minProgressValueView;
+	TextView maxProgressValueView;
+
+	SeekBar seekBar;
+
+	Button setMinButton;
+	Button setMaxButton;
+
+	EditText minEditText;
+	EditText maxEditText;
+
+	ImageButton leftButton;
+	ImageButton rightButton;
+
 	int minValue = 0;
 	int maxValue = 300;
-
+	int shiftProgress = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		//TODO elimina uno dei layout. Questo è per un test.
 		setContentView(R.layout.layouttmp);
 
@@ -31,15 +47,26 @@ public class MainActivity extends Activity {
 		seekBar = (SeekBar) findViewById(R.id.seekBar);
 		seekBar.setMax(maxValue);
 
-		ImageButton leftButton = (ImageButton) findViewById(R.id.leftArrowButton);
-		ImageButton rightButton = (ImageButton) findViewById(R.id.rightArrowButton);
+		minProgressValueView = (TextView) findViewById(R.id.minProgressValueView);
+		maxProgressValueView = (TextView) findViewById(R.id.maxProgressValueView);
+
+		setMinButton = (Button) findViewById(R.id.setMinButton);
+		setMaxButton = (Button) findViewById(R.id.setMaxButton);
+		leftButton = (ImageButton) findViewById(R.id.leftArrowButton);
+		rightButton = (ImageButton) findViewById(R.id.rightArrowButton);
+
+		minEditText = (EditText) findViewById(R.id.minValueInput);
+		maxEditText = (EditText) findViewById(R.id.maxValueInput);
+
+		
+
+
 
 		leftButton.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
 				//I can use a step instead of 1
 				seekBar.setProgress(seekBar.getProgress() - 1);
-
 			}
 		});
 
@@ -49,38 +76,60 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				//I can use a step instead of 1
 				seekBar.setProgress(seekBar.getProgress() + 1);
-
 			}
 		});
+
+
+		setMinButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				try {
+					int min = Integer.parseInt(minEditText.getText().toString());
+					minProgressValueView.setText("" + min);
+					setMinValue(min);
+				} catch (NumberFormatException e){
+					Toast.makeText(getApplicationContext(), R.string.invalid_input_value, Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+		setMaxButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				try {
+					int max = Integer.parseInt(maxEditText.getText().toString());
+					maxProgressValueView.setText("" + max);
+					setMaxValue(max);
+				} catch (NumberFormatException e){
+					Toast.makeText(getApplicationContext(), R.string.invalid_input_value, Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+
 
 		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
-				progressTv.setText("" + (progress+minValue));
-
-
-
+				progressTv.invalidate();
+				progressTv.setText("" + (getShiftProgress() + progress) );
 			}
 		});
-
-
 	}
-
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,4 +138,65 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public int getMinValue() {
+		return minValue;
+	}
+
+	public void setMinValue(int minValue) {
+		int currentShiftProgress = getShiftProgress();
+		if(minValue<getMinValue()){
+			int dif = getMinValue() - minValue;
+			setShiftProgress(currentShiftProgress - dif);
+		} else {
+			int dif = minValue - getMinValue();
+			setShiftProgress(currentShiftProgress + dif);
+		}
+		
+		this.minValue = minValue;
+		seekBar.setMax(getMaxValue() - minValue);
+//		Lascio questa riga se voglio aggiornare lo shift; per mia scelta progettuale azzero il valore in quanto intendo che l'utente lo reimposti secondo il nuovo range
+		Log.d("Mylist", "FGetmin value: " + getMinValue() + " MinValue: " + minValue + " FCurrentShift: " + getShiftProgress() + " Currentshift: " + currentShiftProgress);
+//		seekBar.setProgress(currentProgress + currentShiftProgress);
+		seekBar.setProgress( 0 );
+		progressTv.setText("" + (getShiftProgress()) );
+		
+	}
+
+	public int getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(int maxValue) {
+		int currentShiftProgress = getShiftProgress();
+		int currentMax = getMaxValue();
+		if(maxValue>getMaxValue()){
+			int dif = maxValue - getMaxValue();
+			this.maxValue = getMaxValue() + dif;
+			seekBar.setMax(getMaxValue() + dif);
+//			int dif = getMaxValue() - maxValue;
+//			setShiftProgress(currentShiftProgress + dif);
+		} else {
+			int dif = getMaxValue() - maxValue;
+			this.maxValue = maxValue;
+			seekBar.setMax(maxValue);
+//			int dif = maxValue - getMinValue();
+//			setShiftProgress(currentShiftProgress + dif);
+		}
+		
+		seekBar.setMax(this.maxValue);
+//		Lascio questa riga se voglio aggiornare lo shift; per mia scelta progettuale azzero il valore in quanto intendo che l'utente lo reimposti secondo il nuovo range
+//		seekBar.setProgress(currentProgress + currentShiftProgress);
+		seekBar.setProgress( 0 );
+		progressTv.setText("" + (getShiftProgress()) );
+	}
+
+	public int getShiftProgress() {
+		return shiftProgress;
+	}
+
+	public void setShiftProgress(int shiftProgress) {
+		this.shiftProgress = shiftProgress;
+	}
+
+	
 }
